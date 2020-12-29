c5d5d3307bb4acd272a0d03d4bb7e8eab5281dbd
Nathan Wong <56657800+NathanWong1106@users.noreply.github.com>
2020-12-29 16:16:28 -0500

    Fixed

--- src/liteEngine/engine/Game.java
+++ src/liteEngine/engine/Game.java
@@ -3,7 +3,6 @@ package liteEngine.engine;
 import liteEngine.UI.MainView;
 import liteEngine.UI.ViewSettings;
 import liteEngine.collision.CollisionDispatcher;
-import liteEngine.dataStructures.Vector2;
 import liteEngine.events.UpdateEventSource;
 
 /**
@@ -11,11 +10,6 @@ import liteEngine.events.UpdateEventSource;
  * @author Nathan Wong
  */
 public final class Game {
-	
-	public static void main (String[] args) {
-		init(new ViewSettings(new Vector2(600,600), null, "Hello World!"));
-	}
-	
 	public static void init(ViewSettings viewSettings) {
 		//Call update event first in case any other inits depend on the update loop
 		UpdateEventSource.start();
